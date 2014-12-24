99591e29d7df2112f4a4b566df7a6d48dee1077c
user <82d82a@gmail.com>
2014-12-24 13:14:47 -0500

    Added code to truncate fields description and solution to 32700 bytes due to limitation in Spreadsheet apps. 	modified:   sum_findings.py

--- sum_findings.py
+++ sum_findings.py
@@ -38,17 +38,36 @@ class UnicodeWriter:
 
 
 def write_results(results_table, out_filename):
+    print "\nWriting CSV data..."
     try:
         with open(out_filename, 'wb') as csvfile:
             sum_write = UnicodeWriter(csvfile)
-            for row in results_table:
-                sum_write.writerow(row)
+            sum_write.writerows(results_table)
+
     except IOError as e:
         print "Error writing csv file. Check for permissions and/or path."
         exit()
 
 
+def max_field_len_excel(ggchild, row_number):
+    field = ggchild[1].text
+    if len(field) > 32767:
+        fname = "row.{}.{}.txt".format(row_number, ggchild[0].text)
+        try:
+            with open(fname, "wb") as trunk_file:
+                trunk_file.write(field[32700:])
+        except IOError:
+            print "Error writing remainder of the column data to file. Check for permissions and/or path."
+            exit()
+
+        print "Row number {}, column '{}' was truncated. Remainder can be found in file: {}".format(row_number, ggchild[0].text, fname)
+        return field[:32700]+"[TRUNCATED file:{}]".format(fname)
+    else:
+        return field
+
+
 def get_sum_from_xml(filename):
+    print "\nParsing XML data...\n"
     results_table = [["script_name", "description", "cvss_base_score", "solution"]]
 
     try:
@@ -58,7 +77,9 @@ def get_sum_from_xml(filename):
         print "Error reading/parsing XML file. Likely XML file is mangled in some way. Check the XML file."
         exit()
 
+    row_tracker = 1
     for child in root:
+        row_tracker += 1
         row = ["", "", "", ""]
         for gchild in child:
 
@@ -67,15 +88,15 @@ def get_sum_from_xml(filename):
 
             if gchild.tag == "attributes":
                 for ggchild in gchild:
-
                     if ggchild[0].text == "description":
-                        row[1] = ggchild[1].text
+                        row[1] = max_field_len_excel(ggchild, row_tracker)
 
                     elif ggchild[0].text == "cvss_base_score":
                         row[2] = ggchild[1].text
 
                     elif ggchild[0].text == "solution":
-                        row[3] = ggchild[1].text
+                        row[3] = max_field_len_excel(ggchild, row_tracker)
+
         results_table.append(row)
 
     return results_table
@@ -84,6 +105,8 @@ if __name__ == "__main__":
     if len(sys.argv) == 3:
         results = get_sum_from_xml(sys.argv[1])
         write_results(results, sys.argv[2])
+        print "\nCompleted converting {} rows to CSV format.".format(len(results))
     else:
-        print "USAGE: python {} source_xml_file destination_csv_file".format(sys.argv[0])
+        print "\nUSAGE: python {} source_xml_file destination_csv_file\n " \
+              "Any field longer then 32,700 is truncated and remainder is saved to a txt file.".format(sys.argv[0])
         exit()
